9f13d854bdc3677b30b3cb7cc69873cbb42c9732
Christopher L. Pitt Wolfe <christopher.l.p.wolfe@gmail.com>
2022-12-21 13:30:36 -0500

    Added tendu and tendv to OMP shared list.

--- code/uv_sponge/MOM_hor_visc.F90
+++ code/uv_sponge/MOM_hor_visc.F90
@@ -559,7 +559,8 @@ subroutine horizontal_viscosity(u, v, h, diffu, diffv, MEKE, VarMix, G, GV, US, 
   !$OMP   h_neglect, h_neglect3, inv_PI3, inv_PI6, &
   !$OMP   diffu, diffv, Kh_h, Kh_q, Ah_h, Ah_q, FrictWork, FrictWork_GME, &
   !$OMP   div_xx_h, sh_xx_h, vort_xy_q, sh_xy_q, GME_coeff_h, GME_coeff_q, &
-  !$OMP   KH_u_GME, KH_v_GME, grid_Re_Kh, grid_Re_Ah, NoSt, ShSt &
+  !$OMP   KH_u_GME, KH_v_GME, grid_Re_Kh, grid_Re_Ah, NoSt, ShSt, &
+  !$OMP   tendu, tendv &
   !$OMP ) &
   !$OMP private( &
   !$OMP   i, j, k, n, &
